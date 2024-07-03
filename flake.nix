5526af72250086abd5aa30ea5554af3e3b1c0e01
Jens Plüddemann <jens.plueddemann@novatec-gmbh.de>
2024-07-03 20:08:20 +0200

    cleanup and new github action

---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      packages = forEachSystem (system: {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+      });
+
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+            setupScript = pkgs.writeShellScriptBin "setup-env" "\n";
+          in
+          {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+                  name = "dqualizer development shell";
+
+		              dotenv.disableHint = true;
+                  languages = with pkgs; {
+                    java = {
+                      enable = true;
+                      jdk.package = jdk21;
+                      maven.enable = true;
+                      gradle.enable = true;
+                    };
+                    kotlin.enable = true;
+                  };
+
+                  packages = with pkgs;
+                    [
+                      nushell
+                      nodePackages_latest.nodejs
+                      biome
+                      kotlin-language-server
+                      ktfmt
+                      ktlint
+                    ] ++ lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+                      Cocoa
+                      CoreFoundation
+                      CoreServices
+                      Security
+                    ]);
+
+                  enterShell = "${setupScript}/bin/setup-env";
+                }
+              ];
+            };
+          });
+    };
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,4 @@
+rootProject.name = "dqualizer"
+include("dqapi")
+include("dqexec")
+include("dqtranslator")
